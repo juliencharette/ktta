3435d35d9e167ffd756e796fea93076beaf2c8f8
Julien Charette <hello@juliencharette.com>
2022-11-09 17:22:18 -0500

    Add tailwindcss.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+        './site/templates/*.{twig,php}',
+        './site/snippets/*.{twig,php}',
+        './src/**/*.{css,js}',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
